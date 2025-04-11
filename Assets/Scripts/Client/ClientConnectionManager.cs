d035e242f221bc58aae4bf13f47d3c37ad2dbd76
Xiao Bai <uiojkl3588@qq.com>
2025-04-11 14:51:50 +0800

    creating netcode worlds

--- Assets/Scripts/Client/ClientConnectionManager.cs
+++ Assets/Scripts/Client/ClientConnectionManager.cs
@@ -1,4 +1,7 @@
 ﻿using TMPro;
+using Unity.Entities;
+using Unity.NetCode;
+using Unity.Networking.Transport;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -78,13 +81,38 @@ public class ClientConnectionManager : MonoBehaviour
 
     private static void DestroyLocalSimulationWorld()
     {
+        foreach (var world in World.All)
+        {
+            if (world.Flags == WorldFlags.Game)
+            {
+                world.Dispose();
+                break;
+            }
+        }
     }
 
     private void StartServer()
     {
+        var serverWorld = ClientServerBootstrap.CreateServerWorld("Turbo Server World");
+
+        var serverEndpoint = NetworkEndpoint.AnyIpv4.WithPort(Port);
+        {
+            using var networkDriverQuery =
+                serverWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+            networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Listen(serverEndpoint);
+        }
     }
 
     private void StartClient()
     {
+        var clientWorld = ClientServerBootstrap.CreateClientWorld("Turbo Client World");
+
+        var connectionEndPoint = NetworkEndpoint.Parse(Address, Port);
+        using var networkDriverQuery =
+            clientWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+        networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW
+            .Connect(clientWorld.EntityManager, connectionEndPoint);
+
+        World.DefaultGameObjectInjectionWorld = clientWorld;
     }
 }
